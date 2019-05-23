edb1472e2c0b7b8c70152dc2457704242e9cd212
heidong <heidong@heidongdeMac-mini.local>
2019-05-23 20:53:00 +0800

    加密

--- assets/scripts/scene/SceneCtrl.ts
+++ assets/scripts/scene/SceneCtrl.ts
@@ -7,10 +7,13 @@ import ResManager from "../manager/ResManager";
 import SceneView from "./SceneView";
 
 import CryptoJs = require("../CryptoJS 2");
+import base64 from "../jsencrypt/bin/jsbase64";
 declare let JSEncrypt;
 export default class SceneCtrl extends MVCS.Ctrl {
     private static _instance: SceneCtrl = null;
     private _data: SceneData = new SceneData();
+    aseKey = "1234123412ABCDEF";
+    iv = CryptoJs.enc.Utf8.parse("1234123412ABCDEF");
     constructor(){
         super();
         SceneCtrl._instance = this;
@@ -34,64 +37,137 @@ export default class SceneCtrl extends MVCS.Ctrl {
     createMonster(){
         cc.log("Encrypt" ,new JSEncrypt());
         var encrypt = new JSEncrypt();
-        let pubKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJK5fw7EXnCt3L+z2Azx1h9MXi
-        KqwsnU3c73EcJ8eFU1sLeBHMzQXz0eR85TxwyDASbPDVhrLogNWQ32kHFMlQwtLe
-        tK51LeVxusfvFk87mU5YzSZDd1TEGiQtxIhmbZ3rjDc9e3qVhBswx9PdTmXjrFRd
-        aD0jw3rqBasehCVZbQIDAQAB`;
-        let priKey = `MIICXAIBAAKBgQDJK5fw7EXnCt3L+z2Azx1h9MXiKqwsnU3c73EcJ8eFU1sLeBHM
-        zQXz0eR85TxwyDASbPDVhrLogNWQ32kHFMlQwtLetK51LeVxusfvFk87mU5YzSZD
-        d1TEGiQtxIhmbZ3rjDc9e3qVhBswx9PdTmXjrFRdaD0jw3rqBasehCVZbQIDAQAB
-        AoGBALn5DQvywWSoDQdICkGMNxOZn15IKMdN3O+Eg8KNm8vLlX1y0LHOT9Rpk5tK
-        qKdyyc4e3D3pundqswStJIEhXEEjYvSXtjGIlov4+F18EcFfjqkvZBKwUlsgonE7
-        +WhvvloudXD6o8xL5u5fW0tBp/9ZDLrIOCcoLvpQfIhP/2uZAkEA49fBV95SxrGa
-        PcZQvqUroPfSAR+e41NXRpN+3AGAlZKFVbymKxaLjs4FNIWoZAr4M4aE5pT0egwS
-        XORFjgwZDwJBAOIIAJOJfSlRJZE4HaS2o7fgCxGSGPx4S903wYyLtoo569VY1R9w
-        wwgs4xMmi2Waz+JSBn3gIbJQtGv2E1zPjcMCQEh8ccR0tL0F795LY5Djl3fEoAk/
-        eBs2foOzlR6FU9VCIi1esTbwG0I5hSpqg2Igo0oxN8LsDMNvZ8Ap138Tlj0CQEJs
-        LZctsS1CHnMVdYig722qljYco+EVjN2+c89Ug1kNemNgzoiFv5qYbkaDbjV2kB6n
-        EidNhTl8g/iyALCmgbcCQEhKImBzKSWNeI3B1UW+nMFWb0PRtnhhy1kG0szbceom
-        KhHC4AMGXdKGctg/n7fkzrpRjU10MeeYb55qSJ0a4vI=`;
+        let pubKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC6IfrDS9yvkZh9j8TlI/1/PwhA
+        axH1KY8qB9WS218cF7nWLzHfAidDAY53ZGzlv3M3r8L6/0TpdEWqq+fNuw5tlhdD
+        zYKIcc97IcC/oQt9FUQ8aSPGYkps+3C1o+F/2dvOHq+/pqWZHMTtkLEfuI858NT0
+        X1UTSZEOlsk/nx7h+wIDAQAB`;
+        let priKey = `MIICXQIBAAKBgQC6IfrDS9yvkZh9j8TlI/1/PwhAaxH1KY8qB9WS218cF7nWLzHf
+        AidDAY53ZGzlv3M3r8L6/0TpdEWqq+fNuw5tlhdDzYKIcc97IcC/oQt9FUQ8aSPG
+        Ykps+3C1o+F/2dvOHq+/pqWZHMTtkLEfuI858NT0X1UTSZEOlsk/nx7h+wIDAQAB
+        AoGBALThoJd5eZUEoHQk6uri9s5ZquMvKdxd7sqhomN83Aio5efHJWfa59yt/oz4
+        Avuh+Y38ODdDKuzUGX+qXzIGvhUG+JZ2fptZvP9EVCz1eoRuPItHNb+Uv8CQ7a06
+        mE3NWnDznC9eGmse8yVGas7yyptqAaRuH5Et7pvzjrZdsoOBAkEA7rEfDhkFD6xQ
+        8KIEW+PUELFNxzEMEYU/TnOvHrY/z+lsHvM/8ATNFmUwaQvFJDMGWC3R0se1Fa9L
+        1goqAwcZ0wJBAMehMZT6HWF17qoUd+UjDdvDWqoMKyz7gIJCVKj4JsXm8Rq6NCr/
+        CySj9WQJNg8g4SjzvRhrBwDqDLRrb9smFjkCQFJlJAI60stxQVN/qh+KSuckPWQY
+        Y0EdDRhoyLfCU87SlfctIxIYpi8n6Qkp/ficAIGp93daZwFATN5rDcuTxO8CQFUD
+        Mz8zKDHp7AIoB0rgG7W7wq9p0TUtwXM3ViBC1RgwGfr80tyAkjk/TXAVcB0PLnOn
+        IJ72bRwZj+GBYzgbRxkCQQCMe0xUlKt93E4DEgvsh3WP8WYIEHhKkV/EtmNxSlGK
+        recR/eykucaW/jcy3l/t1vsxdosiLKBxWG6FttK8TNc1`;
 
-        encrypt.setPublicKey(pubKey); // pubkey是公钥内容
+        encrypt.setPublicKey(pubKey);// pubkey是公钥内容
         let testText = JSON.stringify({name: "1"});
-        var encrypted = encrypt.encrypt(testText);
-        cc.log("jiami",encrypted)
-        encrypt.setPrivateKey(priKey);
-        let decr = encrypt.decrypt(encrypted);
-        cc.log("jie", decr)
+        // encrypt.sign(testText);
+        // var encrypted = encrypt.encrypt(testText);
+        // cc.log("jiami",encrypted)
+        // encrypt.setPrivateKey(priKey); 
+        // let decr = encrypt.decrypt(`pR7P0kaZLjUuuEmhGa76mxqrRncPVaM28/krhlJMGFZ/ImAd48QrUMUDTiuTifl64ed0PfWJXJWQqm5e5aG5SvWu39FpR6NR7+dHkMU7t1JB/mszafe26+eYVECCuSwL3btYeMn/RKWLaR/txJZeHFaGhLBbvrKM+nFgBrfZ+xY=`);
+        // cc.log("jie", decr)
 
         cc.log("------CryptoJs-------" ,CryptoJs.AES)
 
         const key = CryptoJs.enc.Utf8.parse("1234123412ABCDEF");
         const iv = CryptoJs.enc.Utf8.parse("ABCDEF1234123412");
 
-        let enc = (data) => {
-            let srcs = CryptoJs.enc.Utf8.parse(data);
-            let encrypted = CryptoJs.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
-            return encrypted.ciphertext.toString().toUpperCase();
-        }
+        // let getAesKey = (account: string = "1234ABCD") => {
+        //     let tempKey = account + Date.now();
+        //     let enKey = enc(tempKey);
+        //     let pre16 = "";
+        //     let nex16 = "";
+        //     let rootKey = "";
+        //     for(let i = 0; i < 16; ++i){
+        //         pre16 += enKey[i];
+        //         nex16 += enKey[i + 16];
+        //     }
+        //     nex16 = base64.encode(nex16);
+        //     let t16 = "";
+        //     for(let i = 0; i < 16; ++i){
+        //         t16 += nex16[i];
+        //     }
+        //     nex16 = t16;
+        //     let date = new Date();
+        //     let day = date.getDate();
+        //     cc.log("day", day % 2);
+        //     rootKey = ((day % 2) == 0) ? pre16 + nex16 : nex16 + pre16;
+        //     return rootKey;
+        // }
 
-        let dec = (data) => {
-            let enHexStr = CryptoJs.enc.Hex.parse(data);
-            let srcs = CryptoJs.enc.Base64.stringify(enHexStr);
-            let dec = CryptoJs.AES.decrypt(srcs, key, {iv: iv, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
-            let decryptedStr = dec.toString(CryptoJs.enc.Utf8);
-            return decryptedStr.toString();
-        }
+        let tk = this.getAesKey();
+
+        // let enc = (data) => {
+        //     let srcs = CryptoJs.enc.Utf8.parse(data);
+        //     let encrypted = CryptoJs.AES.encrypt(srcs, k, { iv: k, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
+        //     return encrypted.ciphertext.toString().toUpperCase();
+        // }
+
+        // let dec = (data) => {
+        //     let enHexStr = CryptoJs.enc.Hex.parse(data);
+        //     let srcs = CryptoJs.enc.Base64.stringify(enHexStr);
+        //     let dec = CryptoJs.AES.decrypt(srcs, k, {iv: k, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
+        //     let decryptedStr = dec.toString(CryptoJs.enc.Utf8);
+        //     return decryptedStr.toString();
+        // }
 
         //en
-        let enText = enc(testText);
+        let enText = this.enc(testText, key);
 
         //de
-        let deText = dec(enText);
+        let deText = this.dec(enText, key);
 
         cc.log("enText", enText)
         cc.log("deText", deText)
+        cc.log("--------deText--------", key)
 
 
+        let enKey = encrypt.encrypt("1234123412ABCDEF");
+        encrypt.setPrivateKey(priKey); 
+
+        let deKey = encrypt.decrypt(enKey);
+        cc.log("enKey", enKey)
+        cc.log("deKey", deKey)
+        cc.log("----------------")
+
+        cc.log(this.dec(enText, CryptoJs.enc.Utf8.parse(deKey)), enText);
         // CryptoJs.algo
         // UIManager.getInstance().getView<SceneView>(ResPath.uiPath.UI_Scene).then((view) => {
         //     view.op();
         // });
+    }
+
+    enc(data, key){
+        let srcs = CryptoJs.enc.Utf8.parse(data);
+        let encrypted = CryptoJs.AES.encrypt(srcs, key, { iv: this.iv, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
+        return encrypted.ciphertext.toString().toUpperCase();
+    }
+    dec = (data, key) => {
+        let enHexStr = CryptoJs.enc.Hex.parse(data);
+        let srcs = CryptoJs.enc.Base64.stringify(enHexStr);
+        let dec = CryptoJs.AES.decrypt(srcs, key, {iv: this.iv, mode: CryptoJs.mode.CBC, padding: CryptoJs.pad.Pkcs7 });
+        let decryptedStr = dec.toString(CryptoJs.enc.Utf8);
+        return decryptedStr.toString();
+    }
+
+
+    getAesKey(account: string = "1234ABCD"){
+        let tempKey = account + Date.now();
+        let enKey = this.enc(tempKey, this.aseKey);
+        let pre16 = "";
+        let nex16 = "";
+        let rootKey = "";
+        for(let i = 0; i < 16; ++i){
+            pre16 += enKey[i];
+            nex16 += enKey[i + 16];
+        }
+        nex16 = base64.encode(nex16);
+        let t16 = "";
+        for(let i = 0; i < 16; ++i){
+            t16 += nex16[i];
+        }
+        nex16 = t16;
+        let date = new Date();
+        let day = date.getDate();
+        cc.log("day", day % 2);
+        rootKey = ((day % 2) == 0) ? pre16 + nex16 : nex16 + pre16;
+        return rootKey;
     }
 }
